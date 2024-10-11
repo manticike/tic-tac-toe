69bf748301afd49396ecf3f6dc477d335a8551e3
manticike <antwikisaac@gmail.com>
2024-10-11 12:02:19 +0000

    Added the method for checking the winning combo This method checks the rows and columns to see if it's covered with all of a symbol

--- lib/board.rb
+++ lib/board.rb
@@ -16,16 +16,46 @@ class Board
 
   # Place the player's move(symbol) on the board
   def place_move(row, col, symbol)
-    grid[row][col] = symbol
-    
-    if valid_move
+    # Check if the user has made a valid move
+    if valid_move?(row, col)
+      grid[row][col] = symbol
+      true
+    else
+      puts "Invalid move! Please enter a valid row and column(0-2) and cell that's empty"
+      false
+    end
+  end
+
+  # Check if the current player has won the game
+  def winning_combination?(symbol)
+    # Check the rows and column for a winner
+    (0..2).each do |i|
+      return true if grid[i][0] == symbol && grid[i][1] == symbol && grid[i][2] == symbol ||
+                     grid[0][i] == symbol && grid[1][i] == symbol && grid[2][i] == symbol
+    end
+
+    # Check main diagonal
+    if grid[0][0] == symbol && grid[1][1] == symbol && grid[2][2] == symbol
+      return true
+    end
+
+    # Check anti-diagonal
+    if grid[0][2] == symbol && grid[1][1] == symbol && grid[2][0] == symbol
+      true
+    end
   end
 
   # Ensuring the player makes a legal move
   def valid_move?(row, col)
     row.between?(0, 2) && col.between?(0, 2) && grid[row][col] == ' '
+  end
 end
 
+symbol = 'X'
+col = 1
+row = 1
 my_board = Board.new
+
+my_board.place_move(row, col, symbol)
 
 my_board.display
